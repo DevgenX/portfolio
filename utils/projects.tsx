e7fbd2b309e331f23bf36fdf25b1653cb3f3c8a0
devgenx <sebastian110420@gmail.com>
2023-05-20 14:36:17 -0700

    fixed photo errors

--- utils/projects.tsx
+++ utils/projects.tsx
@@ -13,7 +13,7 @@ export const Projects: ProjectProps[] = [
     name: "TradingLeague",
     description:
       "TradingLeague is a cryptocurrency trading game with 3 game modes (Normal, PvP and Ranked). The game lets you practice daytrading the most enjoyable and most interactive way.",
-    image: "/tradingleague.png",
+    image: "/assets/tradingleague.png",
     github: "https://github.com/DevgenX/TradingLeague",
     link: "https://tradingleague.onrender.com/",
   },
@@ -21,7 +21,7 @@ export const Projects: ProjectProps[] = [
     name: "Invasion RO",
     description:
       "A web3 blockchain-integrated play-to-earn Ragnarok game with governance and farming tokens that allows users to earn tokens in the game and exchange for other crypto tokens.",
-    image: "/ragnarok.png",
+    image: "/assets/ragnarok.png",
     github:
       "https://polygonscan.com/token/0x8d65d531cec2bd07b468b0dc68a89daa8fcfffa7#code",
     link: "https://marketplace.invasionro.net/",
@@ -30,7 +30,7 @@ export const Projects: ProjectProps[] = [
     name: "Jobby",
     description:
       "Jobby is a job-hunting management tool that allows users to Add, Modify and visualize their job application through charts summarizing their monthly job application statistics.",
-    image: "/jobby.png",
+    image: "/assets/jobby.png",
     github: "https://github.com/DevgenX/Jobby",
     link: "https://jobby-vwkk.onrender.com/",
   },
